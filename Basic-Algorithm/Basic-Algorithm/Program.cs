e26a306cda4e144323955a4543cb5bf41f3c3c76
ahmed-it-ai1 <zamlkawy777@gmail.com>
2024-06-23 04:07:37 +0300

    add selecting() & inter() & PrintTrianglesPatternsV1()

--- Basic-Algorithm/Basic-Algorithm/Program.cs
+++ Basic-Algorithm/Basic-Algorithm/Program.cs
@@ -10,6 +10,43 @@ namespace Basic_Algorithm
     {
         static void Main(string[] args)
         {
+            while (true) {
+                inter();
+                int selcetedNumber = int.Parse(Console.ReadLine());
+                selecting(selcetedNumber);
+            }
         }
+        public static void selecting (int selcetedNumber)
+        {            
+            switch (selcetedNumber)
+            {
+                case 1 :
+                   PrintTrianglesPatternsV1();
+                    break;
+                default:
+                    Console.WriteLine("Invalid number :"+ selcetedNumber);
+                    break;
+            }
+
+        }
+        public static void inter()
+        {
+            Console.WriteLine($"                select number of the Algorithm   ");
+            Console.WriteLine("1- Print Triangles Patterns ( Level 1 ) .");
+            Console.Write("inter your number :");
+        }
+        public static void PrintTrianglesPatternsV1()
+        {
+
+            for (int x = 0; x < 10 ; x++)
+            {
+                Console.WriteLine();
+                for (int i = 0; i < x; i++)
+                {
+                    Console.Write("* ");
+                }
+            }
+        }
+
     }
 }
